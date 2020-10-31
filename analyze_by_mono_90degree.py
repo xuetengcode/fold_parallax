ee084693e9eccab75adb5925c2700bf31c30b893
Xue <Xue@Xues-MacBook-Air.local>
2020-10-30 20:41:12 -0400

    analyze mono at pi/2

--- analyze_by_mono_90degree.py
+++ analyze_by_mono_90degree.py
@@ -9,16 +9,20 @@ import matplotlib.pyplot as plt
 d = 1.4
 f = 1.3
 ipd = 0.064
-gain = 1
-parallax = 1#
-shift = 0#[-1,1]
+gain = 2
+parallax = 1.2# [1,gain]
+shift = 0.2# [0,gain-1]
 p = np.arange(-0.15,0.15,0.001)
 eye = p*parallax+ipd/2
-# In[original line when eye position is (0,0)]
-ar0 = (d+1)/1
-#  x = 0
-al0 = -(d+1)/1
-# In[eye position at (p*parallax+ipd/2,0)], y = ax+b, ar - a for right side point, am - middle, al - left.
+# In[original line when head position is at (0,0), eye at (ipd/2, 0)]
+ar0 = (d+1)/(1-ipd/2)
+am0 = -2*d/ipd
+al0 = -(d+1)/(1+ipd/2)
+
+br0 = -ipd*(d+1)/(2-ipd)
+bm0 = d
+bl0 = ipd*(d+1)/(2+ipd)
+# In[eye position at (p*parallax+ipd/2,0)]
 #passing(p*parallax+ipd/2,0), and (object point)
 #slope
 ar = (d+1)/(1-p*gain-ipd/2+p*shift)
@@ -28,17 +32,16 @@ al = (d+1)/(-1-p*gain-ipd/2+p*shift)
 br=-ar*(p*parallax+ipd/2)
 bm=-am*(p*parallax+ipd/2)
 bl=-al*(p*parallax+ipd/2)
-#focal plane, intersect at z = f, f=a*x+b, x = (f-b)/a
+#intersect at y = f, f=a*x+b, x = (f-b)/a
 xr = (f-br)/ar
 xm = (f-bm)/am
 xl = (f-bl)/al
 # In[] Intersect of original line and y=a*x+b
 #y=y
-#x(d+1)=ar*x+br, x=br/(d+1-ar), y=br(d+1)/(d+1-ar)
-d_perceived_r = br*(d+1)/(d+1-ar)
-d_perceived_m = bm
-d_perceived_l = bl*(-d-1)/(-d-1-al)
-
+#a0*x+b0=a*x+b, x=(b-b0)*a0/(a0-a)+b0
+d_perceived_r = (br-br0)*ar0/(ar0-ar)+br0
+d_perceived_m = (bm-bm0)*am0/(am0-am)+bm0
+d_perceived_l = (bl-bl0)*al0/(al0-al)+bl0
 # In[]
 fig, axs = plt.subplots(1,3, figsize=(20,5))
 #for rw in range(3):
@@ -48,9 +51,9 @@ axs[1].plot(p, d_perceived_m, linestyle='-', color="k", label="Middle point")
 axs[0].legend()
 axs[1].legend()
 axs[2].legend()
-axs[0].set(title="mean = "+str(np.round(np.mean(d_perceived_l),2)) + " m")
+axs[0].set(title="median = "+str(np.round(np.median(d_perceived_l),2)) + " m")
 axs[1].set(title="median = "+str(np.round(np.median(d_perceived_m),2)) + " m")
-axs[2].set(title="mean = "+str(np.round(np.mean(d_perceived_r),2)) + " m")
+axs[2].set(title="median = "+str(np.round(np.median(d_perceived_r),2)) + " m")
 axs[0].set(xlabel='Parallax (Meter)')
 axs[1].set(xlabel='Parallax (Meter)')
 axs[2].set(xlabel='Parallax (Meter)')
@@ -59,18 +62,19 @@ axs[0].set(ylabel='Predicted Distance (Meter)')
 axs[0].set_ylim(0,2.5)
 axs[1].set_ylim(0,2.5)
 axs[2].set_ylim(0,2.5)
-
 # In[]
-def write_csv(variable,csv_name):
-    csv_file = csv_name + '.csv'
-    with open(csv_file, 'w') as f:
-        for i1 in range(variable.shape[0]):
-            if len(variable.shape)>1:
-                for i2 in range(variable.shape[1]):
-                    f.write(str(variable[i1,i2]))
-                    f.write(',')
-            else:
-              f.write(str(variable[i1]))  
-            f.write('\n')
-x_save = np.stack((xr,xm,xl),axis=-1)
-write_csv(x_save,"./gain"+str(gain)+"parallax"+str(parallax)+"shift"+str(shift))
+# =============================================================================
+# def write_csv(variable,csv_name):
+#     csv_file = csv_name + '.csv'
+#     with open(csv_file, 'w') as f:
+#         for i1 in range(variable.shape[0]):
+#             if len(variable.shape)>1:
+#                 for i2 in range(variable.shape[1]):
+#                     f.write(str(variable[i1,i2]))
+#                     f.write(',')
+#             else:
+#               f.write(str(variable[i1]))  
+#             f.write('\n')
+# x_save = np.stack((xr,xm,xl),axis=-1)
+# write_csv(x_save,"./gain"+str(gain)+"parallax"+str(parallax)+"shift"+str(shift))
+# =============================================================================
